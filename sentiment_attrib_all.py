d2ef10de1a463b7aac796e922555fc9d2ea09c1e
yashrajstha <yashrajstha@gmail.com>
2020-04-15 15:46:37 +0200

    Update sentiment_attrib_all.py

--- sentiment_attrib_all.py
+++ sentiment_attrib_all.py
@@ -86,7 +86,7 @@ def figure_plot(title, x, y_list, x_label, y_label, label, color_list, filename=
 	
 if __name__=="__main__":
 
-	data.path.append((r"/home/huangyuanhao/share/nltk_data"))
+	data.path.append((r"/home/xxxxx/share/nltk_data"))
 	sen_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 	sentiment_analyzer = SentimentIntensityAnalyzer()
 	data_file = '20170418_attrib.csv'
